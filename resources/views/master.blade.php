2a28c48c3b604e579a74869e55c9a1c4f0304103
hosseindbk <hosseindbk@gmail.com>
2024-05-06 22:58:09 +0330

    v.0.0.2

---
+++ resources/views/master.blade.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="" >
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="stylesheet" href="{{asset('Site/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('Site/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('Site/css/boxicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('Site/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('Site/css/odometer.min.css')}}">
+    <link rel="stylesheet" href="{{asset('Site/css/meanmenu.css')}}">
+    <link rel="stylesheet" href="{{asset('Site/css/magnific-popup.min.css')}}">
+    <link rel="stylesheet" href="{{asset('Site/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('Site/css/rtl.css')}}">
+    <link rel="stylesheet" href="{{asset('Site/css/responsive.css')}}">
+
+    <title>موسسه فرهنگی هنر شجره طیبه</title>
+
+    <link rel="icon" type="image/png" href="{{asset('Site/img/favicon.png')}}">
+{{--    <link rel="icon" href="{{url($companies['favicon16'])}}" sizes="16x16" type="image/png">--}}
+{{--    <link rel="icon" href="{{url($companies['favicon32'])}}" sizes="32x32" type="image/png">--}}
+
+    @yield('style')
+</head>
+<body>
+
+@yield('main')
+
+<!-- Start Footer Area -->
+<footer class="footer-area">
+    <div class="divider"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <div class="logo">
+                        <a href="index.html"><img src="{{asset('Site/img/white-logo.png')}}" alt="image"></a>
+                    </div>
+                    <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <h3>شرکت</h3>
+
+                    <ul class="services-list">
+                        <li><a href="#">درباره ما</a></li>
+                        <li><a href="#">خدمات</a></li>
+                        <li><a href="#">امکانات</a></li>
+                        <li><a href="#">قیمت ها</a></li>
+                        <li><a href="#">آخرین اخبار</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <h3>پشتیبانی</h3>
+
+                    <ul class="support-list">
+                        <li><a href="#">سوالات متداول</a></li>
+                        <li><a href="#">حریم خصوصی</a></li>
+                        <li><a href="#">شرایط و ضوابط</a></li>
+                        <li><a href="#">انجمن</a></li>
+                        <li><a href="#">تماس با ما</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <h3>اطلاعات تماس</h3>
+
+                    <ul class="footer-contact-info">
+                        <li>موقعیت: <a href="https://goo.gl/maps/MQ78iGP5g9VgZcJ38" target="_blank">کشور شما ، استان و شهر ، محله سکونت شما</a></li>
+                        <li>ایمیل: <a href="mailto:hello@spacle.com">hello@spacle.com</a></li>
+                        <li>تلفن: <a href="tel:021-12345678">021-12345678</a></li>
+                    </ul>
+                    <ul class="social">
+                        <li><a href="#" target="_blank"><i class="bx bxl-facebook"></i></a></li>
+                        <li><a href="#" target="_blank"><i class="bx bxl-twitter"></i></a></li>
+                        <li><a href="#" target="_blank"><i class="bx bxl-linkedin"></i></a></li>
+                        <li><a href="#" target="_blank"><i class="bx bxl-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="copyright-area">
+            <p>کپی رایت <i class="bx bx-copyright"></i> 1402 استراکس. تمام حقوق قالب محفوظ است. طراحی و توسعه توسط <a href="https://www.rtl-theme.com/author/barat/" target="_blank">Barat Hadian</a></p>
+        </div>
+    </div>
+</footer>
+
+<div class="go-top"><i class='bx bx-chevron-up'></i></div>
+
+<script src="{{asset('Site/js/jquery.min.js')}}"></script>
+<script src="{{asset('Site/js/popper.min.js')}}"></script>
+<script src="{{asset('Site/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('Site/js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('Site/js/jquery.appear.min.js')}}"></script>
+<script src="{{asset('Site/js/odometer.min.js')}}"></script>
+<script src="{{asset('Site/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('Site/js/jquery.meanmenu.js')}}"></script>
+<script src="{{asset('Site/js/wow.min.js')}}"></script>
+<script src="{{asset('Site/js/conversation.js')}}"></script>
+<script src="{{asset('Site/js/jquery.ajaxchimp.min.js')}}"></script>
+<script src="{{asset('Site/js/form-validator.min.js')}}"></script>
+<script src="{{asset('Site/js/contact-form-script.js')}}"></script>
+<script src="{{asset('Site/js/particles.min.js')}}"></script>
+<script src="{{asset('Site/js/coustom-particles.js')}}"></script>
+<script src="{{asset('Site/js/main.js')}}"></script>
+
+@yield('script')
+</body>
+</html>
